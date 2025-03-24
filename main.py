1fcb3a4e5788cb2ffed6de904835f70904061f4a
HamedAlashwal <uae14q@gmail.com>
2025-03-24 19:09:12 +0000

    Added Guest class

--- main.py
+++ main.py
@@ -33,3 +33,40 @@ class Room:
 
     def __str__(self):
         return "Room " + str(self.__room_number) + " (" + self.__room_type.value + ") - AED " + str(self.__price_per_night) + "/night, Available: " + str(self.__available)
+
+
+
+class LoyaltyStatus(enum.Enum):
+    REGULAR = "Regular"
+    SILVER = "Silver"
+    GOLD = "Gold"
+    PLATINUM = "Platinum"
+
+class Guest:
+    def __init__(self, guest_id, name, email, phone, loyalty_status):
+        self.__guest_id = guest_id
+        self.__name = name
+        self.__email = email
+        self.__phone = phone
+        self.__loyalty_status = loyalty_status
+
+    def get_guest_id(self):
+        return self.__guest_id
+
+    def get_name(self):
+        return self.__name
+
+    def get_email(self):
+        return self.__email
+
+    def get_phone(self):
+        return self.__phone
+
+    def get_loyalty_status(self):
+        return self.__loyalty_status
+
+    def set_loyalty_status(self, status):
+        self.__loyalty_status = status
+
+    def __str__(self):
+        return "Guest ID: " + str(self.__guest_id) + ", Name: " + self.__name + ", Email: " + self.__email + ", Phone: " + self.__phone + ", Loyalty Status: " + self.__loyalty_status.value
